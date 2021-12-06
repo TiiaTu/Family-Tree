1530e5c3a4b5f4aeb1681e2a8a564cefd0650978
Tiia Tupamäki <tiia.tupamaki@gmail.com>
2021-12-06 18:23:57 +0100

    motherId och fatherId bråkar igen

---
+++ Inlämning2_Tiia/Utils/Helper/PeopleInDatabase.cs
@@ -0,0 +1,53 @@
+﻿using Inlämning2_Tiia.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inlämning2_Tiia.Utils.Helper
+{
+    class PeopleInDatabase
+    {
+        public static void PersonsAdded()
+        {
+            
+
+            CreatePerson("Rhaegar", "Targaryen", 6, 5); //ID = 1
+            CreatePerson("Viserys", "Targaryen", 6, 5); //ID = 2
+            CreatePerson("Daenerys", "Targaryen", 6, 5); //ID = 3
+            CreatePerson("Elia", "Martell", 0, 0);
+            CreatePerson("Aerys", "Targaryen", 0, 0);
+            CreatePerson("Rhaella", "Targaryen", 0, 0);
+            CreatePerson("Lyanna", "Stark", 0, 0);
+            CreatePerson("Jon", "Snow", 7, 1);
+            CreatePerson("Rhaenys", "Targaryen", 4, 1);
+            CreatePerson("Aegon", "Targaryen", 4, 1);
+        }
+        public static Person CreatePerson(string firstName, string lastName, int motherId, int fatherId)
+        {
+
+            using (var db = new PersonContext())
+            {
+                var person = db.People.
+                            FirstOrDefault(
+                                p => p.FirstName == firstName &&
+                                p.LastName == lastName &&
+                                p.MotherId == motherId &&
+                                p.FatherId == fatherId
+                                );
+
+                if (person == null) // skapa personen om den inte finns
+                {
+                    person = new Person { FirstName = firstName, LastName = lastName, MotherId = motherId, FatherId = fatherId };
+
+                    db.People.Add(person);
+                    db.SaveChanges();
+                }
+                return person;
+            }
+        }
+
+
+    }
+}
--- Inlämning2_Tiia/Utils/PersonCrud.cs
+++ Inlämning2_Tiia/Utils/PersonCrud.cs
@@ -13,7 +13,9 @@ namespace Inlämning2_Tiia
         {
             using (var db = new PersonContext())
             {
-                PersonsAdded(); //lägger till alla personer till listan
+                db.Database.EnsureCreated();
+
+                Utils.Helper.PeopleInDatabase.PersonsAdded(); //lägger till alla personer till listan
                 db.SaveChanges();
             }
 
@@ -28,49 +30,47 @@ namespace Inlämning2_Tiia
             //CreatePerson(firstName, lastName);
         }
 
-        private static void PersonsAdded()
-        {
-            CreatePerson("Rhaegar", "Targaryen"); //ID = 1
-            CreatePerson("Viserys", "Targaryen"); //ID = 2
-            CreatePerson("Daenerys", "Targaryen"); //ID = 3
-            CreatePerson("Elia", "Martell");
-            CreatePerson("Aerys", "Targaryen");
-            CreatePerson("Rhaella", "Targaryen");
-            CreatePerson("Lyanna", "Stark");
-            CreatePerson("Jon", "Snow");
-            CreatePerson("Rhaenys", "Targaryen");
-            CreatePerson("Aegon", "Targaryen");
-        }
 
-        public static Person CreatePerson(string firstName, string lastName)
+
+        //--------------skapa en person med för- och efternamn
+        public static Person FindAndCreate(string firstName, string lastName)
         {
             using (var db = new PersonContext())
             {
                 var person = db.People.
                             FirstOrDefault(
-                                p => p.FirstName == firstName && // Sök på namnet
+                                p => p.FirstName == firstName &&
                                 p.LastName == lastName
                                 );
-                if (person == null) // om personen inte finns, skapa den
+
+                if (person == null) // skapa personen om den inte finns
                 {
                     person = new Person { FirstName = firstName, LastName = lastName };
+                    Console.WriteLine($"{firstName} {lastName} is added to the database");
+
                     db.People.Add(person);
-                    db.SaveChanges(); // objektet uppdateras med ID efter save
+                    db.SaveChanges();
+                }
+                else
+                {
+                    Console.WriteLine($"{person.FirstName} {person.LastName} exists in the database and has Id {person.Id}");
                 }
                 return person;
             }
         }
 
+        //---------------söker en person på Id
         public static Person FindPerson(int id)
         {
             using (var db = new PersonContext())
             {
                 var person = db.People.
                             FirstOrDefault(
-                                p => p.Id == id); //sök på Id
+                                p => p.Id == id);
+
                 if (person == null)
                 {
-                    Console.WriteLine("There is no persons with this Id");
+                    Console.WriteLine("There is no person with this Id");
                 }
                 else
                 {
@@ -80,25 +80,25 @@ namespace Inlämning2_Tiia
             }
         }
 
-        public static person findperson(string firstname, string lastname)
-        {
-            using (var db = new personcontext())
-            {
-                var person = db.people.where(p => p.firstname.contains(firstname) ||
-                                            p.lastname.contains(lastname));
+        //public static Person findperson(string firstname, string lastname)
+        //{
+        //    using (var db = new Personcontext())
+        //    {
+        //        var person = db.people.where(p => p.firstname.contains(firstname) ||
+        //                                    p.lastname.contains(lastname));
 
-                if (person == null)
-                {
-                    console.writeline("there is no persons with this name");
-                }
-                else
-                {
-                    displaylist(firstname, lastname)
-                }
-                return person;
+        //        if (person == null)
+        //        {
+        //            console.writeline("there is no persons with this name");
+        //        }
+        //        else
+        //        {
+        //            displaylist(firstname, lastname)
+        //        }
+        //        return person;
 
-            }
-        }
+        //    }
+        //}
 
         public void GetMother(string firstName) //TODO: gör en likadan till fatherId
         {
@@ -127,7 +127,7 @@ namespace Inlämning2_Tiia
                 if (person != null)
                 {
                     var currentMother = person.MotherId;
-                    var currentMotherName = db.People.FirstOrDefault(mom => mom.ID == currentMother);
+                    var currentMotherName = db.People.FirstOrDefault(mom => mom.Id == currentMother);
                     Console.WriteLine($"\n{person}s current mother is {currentMotherName}");
                 }
                 else Console.WriteLine("The person you searched for doesn't exist!");
@@ -135,129 +135,130 @@ namespace Inlämning2_Tiia
             }
         }
 
-        public static Person SetMother(string fname, string lname, string mfname, string mlname)
-        {
-            using (var db = new PersonContext())
-            {
-                //Sök person, hämta ID
-                var person = db.People.FirstOrDefault(p => p.FirstName == fname && p.LastName == lname);
-                if (person != null)
-                {
-                    //Sök persons mamma, hämta mamma ID
-                    var mom = db.People.FirstOrDefault(m => m.FirstName == mfname && m.LastName == mlname);
+        //public static Person SetMother(string fname, string lname, string mfname, string mlname)
+        //{
+        //    using (var db = new PersonContext())
+        //    {
+        //        //Sök person, hämta ID
+        //        var person = db.People.FirstOrDefault(p => p.FirstName == fname && p.LastName == lname);
+        //        if (person != null)
+        //        {
+        //            //Sök persons mamma, hämta mamma ID
+        //            var mom = db.People.FirstOrDefault(m => m.FirstName == mfname && m.LastName == mlname);
 
-                    if (mom != null) //Om mor finns, uppdatera ID kopplingen
-                    {
-                        var currentMother = person.MotherId;
-                        Console.WriteLine($"\nYou just changed {person}'s mother to {mom}!");
-                        person.MotherId = mom.ID;
-                        db.SaveChanges();
-                    }
-                    else Console.WriteLine($"You have to enter a person that exist in the family tree to become {fname} {lname}'s mom!");
-                }
-                else Console.WriteLine("The person you searched for doesn't exist!");
-                return person;
-            }
-        }
+        //            if (mom != null) //Om mor finns, uppdatera ID kopplingen
+        //            {
+        //                var currentMother = person.MotherId;
+        //                Console.WriteLine($"\nYou just changed {person}'s mother to {mom}!");
+        //                person.MotherId = mom.ID;
+        //                db.SaveChanges();
+        //            }
+        //            else Console.WriteLine($"You have to enter a person that exist in the family tree to become {fname} {lname}'s mom!");
+        //        }
+        //        else Console.WriteLine("The person you searched for doesn't exist!");
+        //        return person;
+        //    }
+        //}
 
-        public static Person FindFather(string fname, string lname)
-        {
-            using (var db = new PersonContext())
-            {
-                var person = db.People.FirstOrDefault(p => p.FirstName == fname && p.LastName == lname);
-                if (person != null)
-                {
-                    var currentFather = person.MotherId;
-                    var currentFatherName = db.People.FirstOrDefault(mom => mom.ID == currentFather);
-                    Console.WriteLine($"\n{person}s current mother is {currentFatherName}");
-                }
-                else Console.WriteLine("The person you searched for doesn't exist!");
-                return person;
-            }
-        }
+        //public static Person FindFather(string fname, string lname)
+        //{
+        //    using (var db = new PersonContext())
+        //    {
+        //        var person = db.People.FirstOrDefault(p => p.FirstName == fname && p.LastName == lname);
+        //        if (person != null)
+        //        {
+        //            var currentFather = person.MotherId;
+        //            var currentFatherName = db.People.FirstOrDefault(mom => mom.ID == currentFather);
+        //            Console.WriteLine($"\n{person}s current mother is {currentFatherName}");
+        //        }
+        //        else Console.WriteLine("The person you searched for doesn't exist!");
+        //        return person;
+        //    }
+        //}
 
-        public static Person SetFather(string fname, string lname, string mfname, string mlname)
-        {
-            using (var db = new PersonContext())
-            {
-                //Sök person, hämta ID
-                var person = db.People.FirstOrDefault(p => p.FirstName == fname && p.LastName == lname);
-                if (person != null)
-                {
-                    //Sök persons mamma, hämta mamma ID
-                    var dad = db.People.FirstOrDefault(m => m.FirstName == mfname && m.LastName == mlname);
+        //public static Person SetFather(string fname, string lname, string mfname, string mlname)
+        //{
+        //    using (var db = new PersonContext())
+        //    {
+        //        //Sök person, hämta ID
+        //        var person = db.People.FirstOrDefault(p => p.FirstName == fname && p.LastName == lname);
+        //        if (person != null)
+        //        {
+        //            //Sök persons mamma, hämta mamma ID
+        //            var dad = db.People.FirstOrDefault(m => m.FirstName == mfname && m.LastName == mlname);
 
-                    if (dad != null) //Om mor finns, uppdatera ID kopplingen
-                    {
-                        var currentMother = person.MotherId;
-                        Console.WriteLine($"\nYou just changed {person}'s mother to {mom}!");
-                        person.MotherId = dad.ID;
-                        db.SaveChanges();
-                    }
-                    else Console.WriteLine($"You have to enter a person that exist in the family tree to become {fname} {lname}'s mom!");
-                }
-                else Console.WriteLine("The person you searched for doesn't exist!");
-                return person;
-            }
-        }
-
-
-        public void Read(string firstName, string lastName)
-        {
-            using (var db = new PersonContext())
-            {
-                var person = db.Persons.Where(p => p.FirstName.Contains(firstName))
-                    .ToList(); ; //sök på Id
-
-                if (person == null)
-                {
-                    Console.WriteLine("There is no persons on this Id");
-                }
-                else
-                {
-                    foreach (var item in db.Persons)
-                    {
-
-                    }
-                }
-                return person;
-            }
-        }
-        public void Update()
-        {
-
-        }
-        public void Delete(Person)
-        {
-            using (var db = new PersonContext())
-            {
-                var person = db.People.
-                            FirstOrDefault(
-                                p => p.FirstName == firstName && // Sök på namnet
-                                p.LastName == lastName
-                                );
-                if (person == null) // om personen inte finns, skapa den
-                {
-                    person = new Person { FirstName = firstName, LastName = lastName };
-                    db.People.Add(person);
-                    db.SaveChanges(); // objektet uppdateras med ID efter save
-                }
-                return person;
+        //            if (dad != null) //Om mor finns, uppdatera ID kopplingen
+        //            {
+        //                var currentMother = person.MotherId;
+        //                Console.WriteLine($"\nYou just changed {person}'s mother to {mom}!");
+        //                person.MotherId = dad.ID;
+        //                db.SaveChanges();
+        //            }
+        //            else Console.WriteLine($"You have to enter a person that exist in the family tree to become {fname} {lname}'s mom!");
+        //        }
+        //        else Console.WriteLine("The person you searched for doesn't exist!");
+        //        return person;
+        //    }
+        //}
 
 
-                public void Delete(int id)
-                {
+        //public void Read(string firstName, string lastName)
+        //{
+        //    using (var db = new PersonContext())
+        //    {
+        //        var person = db.Persons.Where(p => p.FirstName.Contains(firstName))
+        //            .ToList(); ; //sök på Id
 
-                }
+        //        if (person == null)
+        //        {
+        //            Console.WriteLine("There is no persons on this Id");
+        //        }
+        //        else
+        //        {
+        //            foreach (var item in db.Persons)
+        //            {
 
-                private static void DisplayList(List<Person> people, string name)
-                {
-                    Console.WriteLine(name);
-                    foreach (var person in people.OrderBy(n => n.LastName).ThenBy(n => n.FirstName))
-                    {
-                        Console.WriteLine(person);
-                    }
-                    Console.WriteLine();
-                }
-            }
-        }
+        //            }
+        //        }
+        //        return person;
+        //    }
+        //}
+        //public void Update()
+        //{
+
+        //}
+        //public void Delete(Person)
+        //{
+        //    using (var db = new PersonContext())
+        //    {
+        //        var person = db.People.
+        //                    FirstOrDefault(
+        //                        p => p.FirstName == firstName && // Sök på namnet
+        //                        p.LastName == lastName
+        //                        );
+        //        if (person == null) // om personen inte finns, skapa den
+        //        {
+        //            person = new Person { FirstName = firstName, LastName = lastName };
+        //            db.People.Add(person);
+        //            db.SaveChanges(); // objektet uppdateras med ID efter save
+        //        }
+        //        return person;
+
+
+        //        public void Delete(int id)
+        //        {
+
+        //        }
+
+        //        private static void DisplayList(List<Person> people, string name)
+        //        {
+        //            Console.WriteLine(name);
+        //            foreach (var person in people.OrderBy(n => n.LastName).ThenBy(n => n.FirstName))
+        //            {
+        //                Console.WriteLine(person);
+        //            }
+        //            Console.WriteLine();
+        //        }
+        //    }
+    }
+}
